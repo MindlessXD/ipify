0d25d9c328c1a4ea46987c355982da015d9aea0f
Mindless <Dgodwin94@gmail.com>
2020-12-31 03:00:22 -0500

    0.1.1
    refactor

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## IPIFY
-Module For Easy Access Of The IPIFY Service
+Module For Easy Streamlined Access Of The IPIFY IPv4 Service
 
 ### Install
 ```
@@ -10,26 +10,14 @@ npm i @mindlessxd/ipify
 
 #### IPv4
 ```javascript
-const ipify = require('ipify')
+const ipify = require('@mindlessxd/ipify');
 
-console.log( ipify.ip4() )
-// Returns Plain Text IPv4
-//=> '8.8.8.8' 
-
-console.log( ipify.ip4("json") )
-// Returns Json Formated IPv4
-//=> {"ip":"98.207.254.136"}
-```
-
-#### IPv6
-```javascript
-const ipify = require('ipify')
-
-console.log( ipify.ip6() )
-// Returns Plain Text IPv6
-//=> '2001:4860:4860::8888'
-
-console.log( ipify.ip6("json") )
-// Returns Json Formated IPv6
-//=> {"ip":"2001:4860:4860::8888"} 
-```
+(async () => {
+	try {
+        let ipv4 = await ipify.ip4()
+        console.log(ipv4)
+	} catch (error) {
+        console.log(error)
+	}
+})();
+````
--- indjex.js
+++ indjex.js
@@ -1,43 +1,19 @@
-const axios = require('axios').default
+const got = require('got')
 
 /**
  * Fuction For Getting Your External IPv4 Address.
  * @function -  Get IPv4
  * @param {string} format - Format Of The Return (Json Or Plain Tesxt)
  */
-module.exports.ip4 = ( format ) => {
+module.exports.ip4 = async ( format ) => {
     let url 
 
     if (format === null) url = "https://api.ipify.org"
     if (format !== "json") url = "https://api.ipify.org"
     if (format === "json") url = "https://api.ipify.org?format=json"
     
-    axios.get(url)
-    .then(function (response) {
-        return response
-    })
-      .catch(function (error) {
-        return new error("Not Available")
-    })
-}
-
-/**
- * Fuction For Getting Your External IPv6 Address.
- * @function -  Get IPv6 
- * @param {string} format - Format Of The Return (Json Or Plain Tesxt)
- */
-module.exports.ip6 = (format) => {
-    let url 
-
-    if (format === null) url = "https://api64.ipify.org"
-    if (format !== "json") url = "https://api64.ipify.org"
-    if (format === "json") url = "https://api64.ipify.org?format=json"
-
-    axios.get(url)
-    .then(function (response) {
-        return response
-    })
-      .catch(function (error) {
-        return new error("Not Available")
-    })
+    const response = await got(url)
+    let ip = response.body
+    
+    return ip
 }
